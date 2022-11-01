08d3ba531e4d095718c3517919ee55fd580d314d
rlagpwls9795 <rlagpwls9795@naver.com>
2022-11-01 17:52:49 +0900

    20221101

---
+++ 3_Javascript/js/01_JS개요.js
@@ -0,0 +1,13 @@
+//js 파일 <script> 태그 내부라고 생각하면 됨
+
+function btnClick3(){
+    alert('externel 버튼이 클릭되었습니다.');
+}
+
+
+function changeColor1(){
+    document.getElementById("box").style.backgroundColor="red";
+}
+function changeColor2(){
+    document.getElementById("box").style.backgroundColor="yellow";
+}
